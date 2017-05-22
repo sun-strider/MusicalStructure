b6e06fff9e84a5e70805b8a3a4ec00b3ff5e77e7
sun-strider <mevola8@gmail.com>
2017-05-22 22:24:01 +0200

    Implementation of additional activities and listeners in MainActivity

--- app/src/main/java/com/example/me74/musicalstructure/MainActivity.java
+++ app/src/main/java/com/example/me74/musicalstructure/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.me74.musicalstructure;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,74 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        // Find the View that shows the playlists category
+        TextView playlists = (TextView) findViewById(R.id.playlists);
+
+        // Set a click listener on that View
+        playlists.setOnClickListener(new View.OnClickListener() {
+            // The code in this method will be executed when that View is clicked on.
+            @Override
+            public void onClick(View view) {
+                Intent playlistsIntent = new Intent(MainActivity.this, PlaylistsActivity.class);
+                startActivity(playlistsIntent);
+            }
+        });
+
+
+        // Find the View that shows the recently played category
+        TextView recentlyPlayed = (TextView) findViewById(R.id.recently_played);
+
+        // Set a click listener on that View
+        recentlyPlayed.setOnClickListener(new View.OnClickListener() {
+            // The code in this method will be executed when that View is clicked on.
+            @Override
+            public void onClick(View view) {
+                Intent recentlyPlayedIntent = new Intent(MainActivity.this, RecentlyPlayedActivity.class);
+                startActivity(recentlyPlayedIntent);
+            }
+        });
+
+        // Find the View that shows the recently played category
+        TextView artists = (TextView) findViewById(R.id.artists);
+
+        // Set a click listener on that View
+        artists.setOnClickListener(new View.OnClickListener() {
+            // The code in this method will be executed when that View is clicked on.
+            @Override
+            public void onClick(View view) {
+                Intent artistsIntent = new Intent(MainActivity.this, ArtistsActivity.class);
+                startActivity(artistsIntent);
+            }
+        });
+
+        // Find the View that shows the recently played category
+        TextView albums = (TextView) findViewById(R.id.albums);
+
+        // Set a click listener on that View
+        albums.setOnClickListener(new View.OnClickListener() {
+            // The code in this method will be executed when that View is clicked on.
+            @Override
+            public void onClick(View view) {
+                Intent albumsIntent = new Intent(MainActivity.this, AlbumsActivity.class);
+                startActivity(albumsIntent);
+            }
+        });
+
+        // Find the View that shows the recently played category
+        TextView songs = (TextView) findViewById(R.id.songs);
+
+        // Set a click listener on that View
+        songs.setOnClickListener(new View.OnClickListener() {
+            // The code in this method will be executed when that View is clicked on.
+            @Override
+            public void onClick(View view) {
+                Intent songsIntent = new Intent(MainActivity.this, SongsActivity.class);
+                startActivity(songsIntent);
+            }
+        });
+
+
     }
 }
